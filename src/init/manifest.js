a2597f8e918bda85b8bbe7c2ad5daf1a39f2eda3
Leonardo Perez <find.leonardo@gmail.com>
2017-03-06 15:14:16 -0500

    Port fix.

--- src/init/manifest.js
+++ src/init/manifest.js
@@ -3,7 +3,7 @@
 module.exports = {
     connections: [
         {
-            port: process.env.PORT || 8031,
+            port: +process.env.PORT || 8031,
             host: '0.0.0.0',
             labels: ['api']
         }
