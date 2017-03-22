e575929a828e5fa34eb76808dc036a3babdb6f92
Leonardo Perez <find.leonardo@gmail.com>
2017-03-22 13:37:18 -0400

    Reconfigured mongo connection.
    Added save endpoint.

--- config.js
+++ config.js
@@ -14,4 +14,11 @@ module.exports = {
         port: 8831,
         host: 'localhost'
     }
+    mongo: {
+        url: "ds123370.mlab.com",
+        port: 23370,
+        u: "sensor-user",
+        p: "diamondsword.987",
+        db: "sensor"
+    }
 };
--- src/services/dbContext.js
+++ src/services/dbContext.js
@@ -1,45 +1,46 @@
 var MongoClient = require('mongodb').MongoClient;
+var Config = require('./../../config').mongo;
+
+const DEFAULT_COLLECTION = 'data';
 
 const connect = (cb) => {
-    MongoClient.connect("mongodb://sensor-user:diamondsword.987@ds123370.mlab.com:23370/sensor", function (err, db) {
-        if (err) { return console.dir(err); }
+    const dbConnection = `mongodb://${Config.u}:${Config.p}@${Config.url}:${Config.port}/${Config.db}`;
+    //"mongodb://sensor-user:diamondsword.987@ds123370.mlab.com:23370/sensor"
+    console.log(dbConnection);
+    
+    MongoClient.connect(dbConnection, function (err, db) {
+        if (err) { return console.log(err); }
         init(db);
         cb(err, db);
     });
 };
 
 const init = (db) => {
-    var collection = db.collection('data');
-    collection.createIndex({ timeStamp: -1 }, function (err, result) { });
+    var collection = db.collection(DEFAULT_COLLECTION);
+    collection.createIndex({ timestamp: -1 }, function (err, result) { });
 };
 
 module.exports = {
     findLast: (cb) => {
         connect(function (err, db) {
-            var collection = db.collection('data');
-            var d = new Date();
-
-            var doc1, doc2, doc3;
-            setTimeout(() => {
-                doc1 = { timeStamp: d.toISOString(), 'temp': '35.9' };
-                setTimeout(() => {
-                    doc2 = { timeStamp: d.toISOString(), 'temp': '34.6' };
-                    setTimeout(() => {
-                        doc3 = { timeStamp: d.toISOString(), 'temp': '40.6' };
-
-                        collection.insert([doc1, doc2, doc3], { w: 1 }, function (err, result) {
-                            collection
-                                .find()
-                                .sort({ timeStamp: -1 })
-                                .toArray(function (error, docs) {
-                                    cb(err, docs[0]);
-                                });
-                        });
-                    }, 5000);
-                }, 5000);
-            }, 5000);
+            var collection = db.collection(DEFAULT_COLLECTION);
+            collection
+                .find()
+                .sort({ timestamp: -1 })
+                .toArray(function (error, docs) {
+                    cb(err, docs[0]);
+                });
         });
     },
-    save: () => {
+    save: (item, cb) => {
+        if (!item.hasOwnProperty('timestamp')) {
+            let d = new Date();
+            item.timestamp = d.toISOString();
+        }
+
+        var collection = db.collection(DEFAULT_COLLECTION);
+        collection.insert(item, { w: 1 }, function (err, result) {
+            cb(err, result);
+        });
     }
 };
